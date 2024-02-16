0fe054c8100d54cd2fa1ece18ab9a213c9baff98
taro <taro.example.com>
2024-02-16 13:05:41 +0900

    トップページの作成

---
+++ src/main/java/com/view/literature/JiasobiApplication.java
@@ -0,0 +1,13 @@
+package com.view.literature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiasobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiasobiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/view/literature/JiasobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.view.literature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiasobiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
